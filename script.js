38610373bf73cea3c97c7a7cab3654e872238f83
Ashika-18 <ashibasama@gmail.com>
2023-07-13 06:09:14 +0900

    easing_direction

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const heading = document.querySelector('#heading');
 
 const keyFrames = {
-
     opacity: [0, 1],
+    translate: ['0 100px', '0 -50px'],
 };
 
-heading.animate(keyFrames, 2000);
+const options = {
+    duration: 2000,
+    direction: 'reverse',
+    easing: 'ease-out',
+};
+
+heading.animate(keyFrames, options);
 
 //準備完了
 console.log('-STANDBY-');
